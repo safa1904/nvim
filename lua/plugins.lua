0c480f59f67ec8a83e5c7eb006e5f4bdc8b90b83
safa1904 <safaabdi96@gmail.com>
2023-02-18 22:59:08 -0700

    discord

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -62,7 +62,8 @@ use {
 }
 -- Git
 	use { "lewis6991/gitsigns.nvim"}
-
+--presence
+     use 'andweeb/presence.nvim'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
