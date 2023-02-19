9266a05a96dedffd8067b8e5ec62684bdef97f2f
safa1904 <safaabdi96@gmail.com>
2023-02-18 18:25:14 -0700

    yahoo

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -13,8 +13,6 @@ vim.api.nvim_set_keymap('n', '<C-j>' , '<C-w>j' , { silent = true})
 vim.api.nvim_set_keymap('n', '<C-k>' , '<C-w>k' , { silent = true})
 vim.api.nvim_set_keymap('n', '<C-h>' , '<C-w>h' , { silent = true})
 
-
-
 --better indenting
 vim.api.nvim_set_keymap('v', '<' , '<gv' , { noremap = true, silent = true})
 vim.api.nvim_set_keymap('v', '>' , '>gv' , { noremap = true, silent = true})
