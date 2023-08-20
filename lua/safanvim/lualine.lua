2d51ce9e9a62bba97d9b1be0e0064eff79fd9ec1
safa1904 <safaabdi96@gmail.com>
2023-08-20 15:14:25 -0600

    test3

--- lua/safanvim/lualine.lua
+++ lua/safanvim/lualine.lua
@@ -8,6 +8,7 @@ function M.config()
         return
     end
 
+
     local funcs = require("utils.functions")
     local daylight = funcs.daylight() -- check timei
      local function day_icon_max()
