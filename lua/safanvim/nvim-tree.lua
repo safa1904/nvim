f7ec668acf82d8d921fa7a6a673b297264fda381
safa1904 <safaabdi96@gmail.com>
2023-08-22 11:02:37 -0600

    update nvim-tree

--- lua/safanvim/nvim-tree.lua
+++ lua/safanvim/nvim-tree.lua
@@ -7,61 +7,50 @@ local M = {
 	event = "VeryLazy",
 }
 function M.config()
-	local setup, nvimtree = pcall(require, "nvim-tree")
+    local setup, nvimtree = pcall(require, "nvim-tree")
 	if not setup then
 		return
 	end
-
-	-- recommended settings from nvim-tree documentation
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwPlugin = 1
-
-	-- change color for arrows in tree to light blue
-	vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
-
-	-- configure nvim-tree
-	nvimtree.setup({
-		-- change folder arrow icons
-		renderer = {
-			icons = {
-				glyphs = {
-					folder = {
-						arrow_closed = "+", -- arrow when folder is closed
-						arrow_open = "-", -- arrow when folder is open
-					},
-				},
-			},
-		},
-		-- disable window_picker for
-		-- explorer to work well with
-		-- window splits
-		actions = {
-			open_file = {
-				window_picker = {
-					enable = false,
-				},
-			},
-		},
-		git = {
-			ignore = true,
-		},
-	disable_netrw = false,
-    hijack_netrw = true,
-    respect_buf_cwd = true,
-    view = {
-      number = false,
-      relativenumber = false,
+    vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+    nvimtree.setup({
+    disable_netrw = false,
+	view = {
+      width = 30,
     },
     filters = {
       custom = { ".git" },
     },
     sync_root_with_cwd = true,
     update_focused_file = {
-      enable = true,
-      update_root = true,
+	    enable = true,
+	    update_root = true,
     },
-	})
-         
+
+    renderer = {
+      icons = {
+        glyphs = {
+          default = "",
+          symlink = "",
+          git = {
+            unstaged = "✗",
+            staged = "✓",
+            unmerged = "",
+            renamed = "➜",
+            untracked = "★",
+          },
+          folder = {
+            arrow_closed = "+",
+	    arrow_open = "-",
+          },
+        },
+      },
+      indent_markers = {
+        enable = false,
+      },
+	
+    },
+	
+  })
 
 	vim.keymap.set("n", "<Leader>e", ":NvimTreeFindFileToggle<CR>")
 end
